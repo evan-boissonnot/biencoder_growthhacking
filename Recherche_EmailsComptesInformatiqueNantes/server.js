14bef173910ec7ef89ec4ad74da92319a7e99a87
evanboissonnot <evan.boissonnot@netil.fr>
2017-07-22 16:09:07 +0200

    Fin première étape de récupération des entreprises

--- Recherche_EmailsComptesInformatiqueNantes/server.js
+++ Recherche_EmailsComptesInformatiqueNantes/server.js
@@ -2,16 +2,8 @@ const loader = require('./DataLoader.js');
 const osmosis = require('osmosis');
 const url = 'https://www.pagesjaunes.fr/annuaire/chercherlespros?quoiqui=informatique%20editeurs%20de%20logiciels%20societes%20de%20services&ou=nantes-44&proximite=0&quoiQuiInterprete=informatique%20editeurs%20de%20logiciels%20societes%20de%20services&contexte=zuMKNmgmkDnVE2vSrKuSQneKL16bkxx3e0d5jKAkSaA%3D&idOu=Z04410900&page=';
 
-osmosis
-    .get('https://www.google.fr/search?q=entreprise+informatique+nantes&oq=entreprise+infor')
-    .find("#ires")
-    .set({ 'related': ['h3 a'] }) // on recherche un tableau de a (et l'on va prendre son inner html)
-    .data(function(data) {
-        console.log(data);
-    });
 
-
-function loadData(pageIndex) {
+function loadData(pageIndex, callback) { // on ajoute le callback
     let save = loader.save.bind(loader);
 
     osmosis
@@ -21,9 +13,21 @@ function loadData(pageIndex) {
        .data(function(data) {
            if(data.related.length > 0)  {
               save(data);
-              loadData(pageIndex + 1);
+
+              console.log(`Traitement de la page ${pageIndex} fini`);
+
+              loadData(pageIndex + 1, callback); // nous avons oublié de le passer ici !
            }
+
+           // s'il n'y a plus rien, on va pouvoir appeler le callback
+           if(data.related.length === 0) 
+                callback(loader.companies);
        });    
 }
 
-loadData(1);
+loadData(1, computeData); // on le passe à la fonction de chargement
+
+/// Notre fonction de traitement final
+function computeData(data) {
+    console.log('on a fini le chargement');
+}
